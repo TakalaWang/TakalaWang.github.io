d1f9fec6a0be8eb6691b3d3c156ed6720b4f1972
Takala <dylan91011028@gmail.com>
2025-07-29 14:51:28 +0800

    feat: update subtitle in site configuration to remove NLP reference

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
   title: "Takala's Blog",
-    subtitle: "A passionate ML & NLP Researcher and Developer && Forever Cataholic",
+    subtitle: "A passionate ML Researcher and Developer && Forever Cataholic",
   lang: "en", // 'en', 'zh_CN', 'zh_TW', 'ja'
   themeColor: {
       hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
